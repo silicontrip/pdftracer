e42235f853d618b78e96ed5f2f6d32a495ed2b24
Mark Heath <silicontrip@gmail.com>
2018-11-15 17:27:53 +1100

    Changes for progress metering.

--- test.java
+++ test.java
@@ -5,7 +5,10 @@ public class test {
 	public static void main(String[] args) {
 		try {
 			Filelist fl = new Filelist(".");
+			System.out.println("-- SCAN --");
 			fl.filescan();
+			System.out.println("Files to Compare: " + fl.countfiles());
+			System.out.println("-- COMPARE --");
 			fl.filecompare();
 			fl.print();
 		} catch (Exception e) {
