680caa60a4661e0c2951f3253c85cf4e277bfe24
Mark Heath <silicontrip@gmail.com>
2018-11-19 13:30:19 +1100

    Made: reset, print, compare and delete commands. does check that not all matching files are deleted, but use at own risk.

--- dupefind.java
+++ dupefind.java
@@ -1,7 +1,41 @@
 import java.util.ArrayList;
+
+import javax.lang.model.util.ElementScanner6;
+
 import java.io.*;
 
 public class dupefind {
+
+	private static boolean findFile (ArrayList<File> sl, ArrayList<File> al)
+	{
+		// any entry in al is found in sl
+		for  (File f : al)
+			if (sl.contains(f))
+				return true;
+
+		return false;
+	}
+
+	private static ArrayList<File> findEntry(ArrayList<File> al, ArrayList<ArrayList<File>> aaf)
+	{
+		int count = 0;
+		ArrayList<File> entry = null;
+		for (ArrayList<File> sl: aaf)
+		{
+			if (findFile(sl,al))
+			{
+				count ++;
+				entry = sl;
+				// will return entry here, but doing testing first.
+			}
+
+		}
+		assert(count <= 1); 
+		// 0 <= count <= 1
+		//if (count == 0)
+			
+		return entry;
+	}
 
 	private static String readln() {
 		String ln = "";
@@ -16,12 +50,12 @@ public class dupefind {
 		return ln;
 	}
 
-        public static void print(ArrayList<ArrayList<File>> duplicatelist)
-        {
-                for (ArrayList<File> al : duplicatelist)
-                {
-                        boolean first = true;
-                        for (File fl : al)
+		private static void printLine (ArrayList<File> al)
+		{
+			if (al != null )
+			{
+			boolean first = true;
+            for (File fl : al)
                         {
                                 if(!first)
                                         System.out.print(" : ");
@@ -32,13 +66,26 @@ public class dupefind {
                                         System.out.print("*ERROR*");
                                 }
                         }
-                        System.out.println("");
-                }
+						System.out.println("");
+					}
+					else      
+					System.out.println("null");
+		}
+        public static void print(ArrayList<ArrayList<File>> duplicatelist)
+        {
+				for (ArrayList<File> al : duplicatelist)
+					printLine(al);
         }
 	public static void main(String[] args) {
 		try {
 			Rulelist  rulelist  = new Rulelist();
-			Filelist fl = new Filelist(".");
+			//System.out.println("args len:" + args.length);
+			Filelist fl;
+			if (args.length == 0)
+				fl = new Filelist(".");
+			else 
+				fl = new Filelist (args[0]);
+
 			System.out.println("-- SCAN --");
 			// start this in thread
 
@@ -52,14 +99,14 @@ public class dupefind {
 			// loop progress...
 			while (!fl.getScanCompleted()) { 
 				try {
-					System.out.println("scanning... " + fl.getScanRemain());
+					System.out.println("Files Remaining... " + fl.getScanRemain());
 					Thread.sleep(1000); 
 				} catch (InterruptedException ie) { ; }
 			}
 		
 			int totalfiles = fl.countfiles();
 
-			System.out.println("Files to Compare: " + totalfiles);
+			System.out.println("Files Remaining to Compare: " + totalfiles);
 			System.out.println("-- COMPARE --");
 			// start this in thread
 
@@ -84,14 +131,64 @@ public class dupefind {
 			ArrayList<ArrayList<File>> currentList = allList;
 
 			while (true) {
-				print(currentList);
-
-				System.out.print("> ");	
+				
+				System.out.print("" + currentList.size() + "> ");	
 				String argument = readln();
 
 				// going to need a command parser class...
-				String[] arguments = argument.split(":");	
-				if (arguments.length == 3) {
+				String[] arguments = argument.split(":");
+				if (arguments.length == 1) {
+					if ("reset".equals(arguments[0]))
+					{
+						currentList = allList;
+					} else if ("delete".equals(arguments[0])) {
+						// check current delete list with alllist
+						ArrayList<ArrayList<File>> newList = new ArrayList<ArrayList<File>>();
+						for (ArrayList<File> al : allList)
+						{
+							ArrayList<File> deleteList = findEntry(al,currentList);
+							if (deleteList != null && deleteList.size() < al.size())
+							{
+								for (File f: deleteList)
+								{
+									System.out.println("Deleting... " + f.getCanonicalPath());
+									f.delete();
+								}
+								// find entry in alllist
+								// count entries
+								// if al.size < alllist.entry.size()
+								// delete
+
+								//
+							} else {
+								if (deleteList != null) {
+									System.out.print("WARNING not deleting all: "  );
+									printLine(deleteList);
+								}
+								newList.add(al);
+							}
+						}
+						allList = newList;
+						currentList = newList;
+					} else if ("compare".equals(arguments[0])) {
+						for (ArrayList<File> al : allList)
+						{
+							ArrayList<File> deleteList = findEntry(al,currentList);
+							if (deleteList != null)
+							{
+								if (al.size() == deleteList.size())
+									System.out.println("-- ALL --");
+								else
+									System.out.println("--");
+								printLine(al);
+								printLine(deleteList);
+							}
+						}
+					
+					} else if ("print".equals(arguments[0])) {
+						print(currentList);
+					}
+				} else if (arguments.length == 3) {
 					if ("group".equals(arguments[0]))
 					{
 						currentList = rulelist.evalGroup(currentList, arguments[1],arguments[2]);
