1c06652e0c41854f54dd10e47531dc8e50c21fb6
mark heath (@somnio) <silicontrip@gmail.com>
2021-02-13 10:42:49 +1100

    code cleanup

--- pdftracer/QPDFDocument.h
+++ pdftracer/QPDFDocument.h
@@ -10,9 +10,6 @@
 #import <Quartz/Quartz.h>
 #import "QPDFObjc.h"
 
-//#import "QPDFDocumentController.h"
-
-
 @class QPDFWindowController;
 
 NS_ASSUME_NONNULL_BEGIN
@@ -21,25 +18,23 @@ NS_ASSUME_NONNULL_BEGIN
 {
 	@private
 	PDFDocument* pDoc;
-	NSData *contentData;
+	// NSData *contentData;
+	QPDFObjc* qDocument;
+	// QPDFWindowController* winCon;  // yet another instance variable handled by super
 }
 
 - (instancetype)init;
 - (instancetype)initWithContentsOfURL:(NSURL *)url ofType:(NSString *)typeName error:(NSError * _Nullable *)outError;
+- (void)saveDocument:(nullable id)sender;
 - (BOOL)readFromURL:(NSURL *)url ofType:(NSString *)typeName error:(NSError * _Nullable *)outError;
 - (BOOL)writeToURL:(NSURL *)url ofType:(NSString *)typeName error:(NSError * _Nullable *)outError;
 - (NSData *)dataOfType:(NSString *)typeName error:(NSError **)outError;
 - (BOOL)readFromData:(NSData *)data ofType:(NSString *)typeName error:(NSError **)outError;
 - (PDFDocument*)pdfdocument;
 - (QPDFObjc*)doc;
-- (NSString*)filePath;
+- (NSString*)displayName;
 + (BOOL)autosavesInPlace;
 
-/*
-- (OutlineQPDF*)pdfDataSource;
-- (OutlinePDFObj*)pdfObjDataSource;
-- (OutlinePDFPage*)pdfPageDataSource;
-*/
 @end
 
 NS_ASSUME_NONNULL_END
