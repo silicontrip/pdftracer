be4150a6ba050fe38f00d60902d11725d19ed3ad
mark heath (@somnio) <silicontrip@gmail.com>
2021-02-16 08:15:05 +1100

    debugging class.

--- pdftracer/QPDFOutlineView.m
+++ pdftracer/QPDFOutlineView.m
@@ -12,16 +12,37 @@
 {
 	NSLog(@"BECOME FR: %@",self);
 	
-	
 	[super becomeFirstResponder];
+	
 	NSResponder* fr = [[self window] firstResponder];
 	do {
 		NSLog(@"resp: %@",fr);
 		fr = [fr nextResponder];
 	} while (fr != nil);
+	
+	NSWindow* w = [self window];
+	NSWindowController* wc = [w windowController];
+	
+	NSLog(@" window: %@ controller: %@",w,wc );
+	
 	[[[self window] windowController] selectChangeNotification:self];
 	
 	return YES;
 }
 
+/*
+- (BOOL)respondsToSelector:(SEL)aSelector
+{
+	NSString* selstr =NSStringFromSelector(aSelector);
+	if (![selstr isEqualToString:@"validModesForFontPanel:"])
+	{
+		NSLog(@"WC EVENT -> %@",NSStringFromSelector(aSelector));
+		if( [NSWindowController instancesRespondToSelector:aSelector] ) {
+			// invoke the inherited method
+			return YES;
+		}
+	}
+	return NO;
+}
+*/
 @end
